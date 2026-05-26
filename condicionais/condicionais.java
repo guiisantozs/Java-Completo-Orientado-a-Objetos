0accaaf0c7fd4a1a9c771cdd79a360d1987bf15c
Guilherme Santos <gui044santos@gmail.com>
2026-05-26 16:11:12 -0300

    condicionais

---
+++ condicionais/condicionais.java
@@ -0,0 +1,40 @@
+package condicionais;
+
+public class condicionais {
+
+    public static void main(String[] args) {
+
+        // 1 - O que é boolean?
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+
+        // Operadores de comparação;
+
+        int x = 10;
+        System.out.println(x == 10);
+        System.out.println(x == 9);
+        System.out.println(x > 9);
+        System.out.println(x < 9);
+        System.out.println(x >= 10);
+        System.out.println(x <= 10);
+        System.out.println(x != 1);
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+    
+}
